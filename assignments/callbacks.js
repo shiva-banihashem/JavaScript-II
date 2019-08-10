21a25a73b533bf47511f8c6b4a89afa37457b304
shiva banihashem <sbanihas@gmail.com>
2019-08-10 00:41:06 -0500

    Strech goals included

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,29 +27,94 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+
+getLength(items,function(length){
+      console.log(length);
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+last(items,function(item) {
+  console.log(item);
+});
 
+
+let num1 =3;
+let num2 = 4;
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+  cb(x+y);
+};
+
+sumNums(num1,num2,(sum =>console.log(sum)));
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  cb(x*y);
+};
+
+multiplyNums(num1,num2,(product =>console.log(product)));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (i=0;i<list.length && bool===false;i++){
+    console.log(item + list[i]);
+   if (item === list[i]) {
+     bool=true;
+   }
+    else {
+      bool=false;
+    }
+  }    
+  cb(bool)  ; 
 }
-
+contains(items[1],items,(bool1 => console.log(bool1)));
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
+// function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+//}
+
+function removeDuplicates(array, cb) 
+{
+ let found=1;
+  let newlist = [];
+  array = items;
+  newlist[0]=array[0];
+ 
+  for(let i=1;i<array.length;i++) {
+     
+     for (let j=0;j<newlist.length && found !==2;j++){  
+       
+       if (array[i] !== newlist[j]){
+         found=1;
+        }
+       else {
+         found= 2;
+       };
+       
+     };
+      if (found ===1){
+        
+        newlist.push(array[i]);
+        
+       };  
+    found = 0;  
+  };
+   cb(newlist);
 }
+ 
+ 
+removeDuplicates(items,(noDup => console.log(noDup)));
+
+
